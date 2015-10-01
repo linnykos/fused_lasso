53bd6b0ab8f1dbb1a585607c14b6be8c0acf0690
Kevin Lin <kevinl1@andrew.cmu.edu>
2015-10-01 11:22:22 -0400

    Lots of new figures

--- dualplot_simulation_2015_10_01.R
+++ dualplot_simulation_2015_10_01.R
@@ -9,12 +9,37 @@ n.vec = seq(100,1000,length.out=n.length)
 jump.mean.org =     c(0, 2,  4, 1, 4)
 jump.location.org = c(0, .2, .4, .6, .8)
 
-
+#scale signal at log n, lambda at sqrt(n log n)
 for(i in 1:n.length){
   set.seed(10)
   y = generate.problem(n.vec[i], jump.location.org, jump.mean.org*log(n.vec[i]), sigma)
-  png(paste("simul_",date,"_lambda-sqrtnlogn_scale-logn_n-",n.vec[i],".png",sep=""), width=8, height=3, units="in", res=300)
+  png(paste("figures/simul_",date,"_lambda-sqrtnlogn_scale-logn_n-",n.vec[i],".png",sep=""), width=8, height=3, units="in", res=300)
   dualplot_suite(y, sqrt(n.vec[i]*(log(n.vec[i])+1)*sigma),jump.mean.org*log(n.vec[i]),jump.location.org) 
   dev.off()
+
+  print("*")
 }
+
+#scale signal at log n, lambda at sqrt(log n)
+for(i in 1:n.length){
+  set.seed(10)
+  y = generate.problem(n.vec[i], jump.location.org, jump.mean.org*log(n.vec[i]), sigma)
+  png(paste("figures/simul_",date,"_lambda-sqrtlogn_scale-logn_n-",n.vec[i],".png",sep=""), width=8, height=3, units="in", res=300)
+  dualplot_suite(y, sqrt((log(n.vec[i])+1)*sigma),jump.mean.org*log(n.vec[i]),jump.location.org)
+  dev.off()
+
+  print("*")
+}
+
+#don't scale signal, lambda at sqrt(log n)
+for(i in 1:n.length){
+  set.seed(10)
+  y = generate.problem(n.vec[i], jump.location.org, jump.mean.org, sigma)
+  png(paste("figures/simul_",date,"_lambda-sqrtlogn_n-",n.vec[i],".png",sep=""), width=8, height=3, units="in", res=300)
+  dualplot_suite(y, sqrt((log(n.vec[i])+1)*sigma),jump.mean.org,jump.location.org)
+  dev.off()
+
+  print("*")
+}
+
 
