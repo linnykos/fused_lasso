089193156f9980f369faac9fab039b752b2ba409
Kevin Lin <kevinl1@andrew.cmu.edu>
2015-10-23 10:25:24 -0400

    Update code to handle thresholding and forming the difference mat
    Altered simulations to handle the new interface of the functions
      (such as jump.means and then jump.location). Split the source
      file into separate files, including now commands to store
      the result as a object with functions to compute MSE.
    
    Source_threshold now includes various ways to threshold the
      resulting fused lasso estimate. Extensive simulations based on
      these methods still need to be done.

---
+++ source.R
@@ -0,0 +1,106 @@
+library(genlasso)
+library(assertthat)
+source("source_plotter.R")
+
+generate.problem <- function(n, jump.mean, jump.location,  sigma = 0, random.seq = NA){
+  #convert jump.location into integer indices
+  tmp = seq(0,1,length.out=n)
+  jump.location = sapply(jump.location,function(x){min(which(tmp>=x))})
+  jump.location[1] = 1
+  
+  y = rep(0,n)
+  for(i in 1:(length(jump.location)-1)){
+    tmp = jump.location[i]:(jump.location[i+1]-1)
+    y[tmp] = rnorm(length(tmp),mean = jump.mean[i], sd = sigma)
+  }
+  tmp = jump.location[length(jump.location)]:n
+  y[tmp] = rnorm(length(tmp),mean = jump.mean[length(jump.mean)], sd = sigma)
+  
+  if(!missing(random.seq)){
+    y = y+random.seq
+  }
+  
+  y
+}
+
+
+dualplot_suite <- function(y, jump.mean, jump.location, lambda = NA, 
+                           num.est.jumps = NA, plot.res = TRUE,
+                           fused.lasso.obj = NA, return.fusedlasso = FALSE){
+  assert_that(!is.na(lambda) || !is.na(num.est.jumps))
+  
+  if(is.na(fused.lasso.obj[1])) {
+    fres = fusedlasso1d(y)
+  } else {
+    fres = fused.lasso.obj
+  }
+  
+  if(!is.na(lambda)) {
+    tmp = coef(fres,lambda=lambda)
+    res = tmp$beta
+  } else {
+    tmp = coef(fres,df=num.est.jumps+1)
+    res = tmp$beta
+    lambda = tmp$lambda
+  }
+
+  ret = summarize(res, jump.mean, jump.location, lambda)
+  
+  if(plot.res) plotfused(jump.mean,jump.location,y,ret[["coef"]],lambda,ret[["jumps"]],ret[["mse"]])
+  
+  ret
+}
+
+summarize <- function(fit, jump.mean, jump.location, lambda){
+  mse = compute.mse(fit, jump.mean, jump.location)
+  num.est.jumps = count.jumps(fit)
+  
+  ret = list()
+  class(ret) = "fusedLassoSummary"
+  ret[["coef"]] = fit
+  ret[["lambda"]] = lambda
+  ret[["jumps"]] = num.est.jumps
+  ret[["mse"]] = mse
+  
+  return(ret)
+}
+
+
+compute.mse <- function(res, jump.mean=NA, jump.location=NA, true.seq=NA){
+  bool1 = !is.na(jump.mean[1]) & !is.na(jump.location[1])
+  bool2 = !is.na(true.seq[1])
+  assert_that(bool1 | bool2)
+  
+  n = length(res)
+  
+  if(bool1) true.seq = form.truth(jump.mean, jump.location, n)
+  
+  sum((true.seq-res)^2)/n
+}
+
+count.jumps <- function(res, tol=1e-3){
+  length(which(abs(diff(res))>tol))
+}
+
+form.truth <- function(jump.mean, jump.location, n){
+  assert_that(min(jump.location)>=0)
+  assert_that(max(jump.location)<=1)
+  
+  tmp = seq(0,1,length.out=n)
+  jump.location2 = sapply(jump.location,function(x){max(min(which(tmp>=x)),1)-1})
+  jump.location2[1] = 1
+  jump.location2 = sort(jump.location2)
+  jump.location3 = c(jump.location2,n)
+  jump.location3[1] = 0
+  
+  rep(jump.mean,times=diff(jump.location3))
+}
+
+form.diff.matrix <- function(n){
+  res = matrix(0,n-1,n)
+  for(i in 1:(n-1)){
+    res[i,c(i,i+1)] = c(-1,1)
+  }
+  
+  res
+}
---
+++ source_plotter.R
@@ -0,0 +1,73 @@
+library(genlasso)
+library(assertthat)
+
+plot.helper <- function(jump.location, jump.mean, n, col = "black", lwd = 3, lty = 1){
+  
+  len = length(jump.location)
+  if(len>1){
+    for(i in 1:(len-1)){
+      lines(x=c(jump.location[i],jump.location[i+1]),y=rep(jump.mean[i],2),col=col,lwd=lwd,lty=lty)
+    }
+  } 
+  lines(x=c(jump.location[len],n),y=rep(jump.mean[len],2),col=col,lwd=lwd,lty=lty)
+  
+  invisible()
+}
+
+
+plotfused <- function(jump.mean, jump.location, y, res, lambda, 
+                      num.est.jumps, mse = NA, tol = 1e-7){
+  par(mfrow=c(2,1),mar=c(1,1,1,1))
+  plot(y,col=rgb(.5,.5,.5),pch=16,cex=1.25)
+  n = length(y)
+  
+  true.seq = form.truth(jump.mean, jump.location, n)
+  if(is.na(mse)) mse = compute.mse(res, true.seq = true.seq)
+  
+  #plot truth
+  tmp = seq(0,1,length.out=n)
+  jump.location2 = sapply(jump.location,function(x){max(min(which(tmp>=x)),1)-1})
+  jump.location2[1] = 1
+  jump.location2 = sort(jump.location2)
+  plot.helper(jump.location2, jump.mean, n, col="red")
+  
+  
+  tmpdiff = diff(res)
+  tmpdiff[abs(tmpdiff)<tol] = 0
+  res.jumploc = c(1,(which(abs(tmpdiff)>tol)))
+  res.jumpmean = res[res.jumploc+1]
+  res.jumploc = sort(res.jumploc)
+  plot.helper(res.jumploc, res.jumpmean, n, col=rgb(0,1,0))
+  
+  #put text up for a pseudo-y-axis
+  tmp.up = round(median(y)+diff(range(y))*.4,2)
+  tmp.down = round(median(y)-diff(range(y))*.4,2)
+  text(x=0, y=tmp.up, labels=as.character(tmp.up),col="red")
+  text(x=n, y=0, labels=as.character(0),col="red")
+  text(x=0, y=tmp.down, labels=as.character(tmp.down),col="red")
+  
+  #plot dual
+  tmp = res-y
+  z = cumsum(tmp)
+  plot(z,ylim=c(-1.5*lambda,1.5*lambda),col=rgb(.5,.5,.5),pch=16)
+  lines(z,col="blue",lwd=2)
+  lines(x=c(-100,n+100),y=rep(lambda,2),lty=2,lwd=2,col="red")
+  lines(x=c(-100,n+100),y=rep(-lambda,2),lty=2,lwd=2,col="red")
+  for(i in 1:length(jump.location2)){
+    lines(x=rep(jump.location2[i],2),y=c(-5*lambda,5*lambda),lty=2,lwd=2,col="red")
+  }
+  
+  tmp = which(abs(abs(z)-lambda)<tol)
+  for(i in 1:length(tmp)){
+    if(z[tmp[i]]>0) {
+      lines(x=rep(tmp[i],2),y=c(lambda,5*lambda))
+    } else {
+      lines(x=rep(tmp[i],2),y=c(-lambda,-5*lambda))
+    } 
+  }
+  
+  #some basic text on the bottom (mse, lambda, n)
+  text(x=0,y=-1.2*lambda,labels=paste("MSE: ", round(mse,3)," // Lambda: ",round(lambda,2)," // num.est.jumps: ",num.est.jumps,sep=""),pos=4)
+  
+  invisible()
+}
