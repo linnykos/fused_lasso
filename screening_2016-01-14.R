abd210ec31ad23f1a382a3e97ef35d1bda67208a
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-02-09 23:30:36 -0500

    Added parallel computing to screening and also plotting

--- screening_2016-01-14.R
+++ screening_2016-01-14.R
@@ -1,6 +1,8 @@
 rm(list=ls())
 source("source_header.R")
 
+registerDoMC(cores = 12)
+
 sigma = 1
 n.length = 10
 n.vec = round(10^seq(2, 4, length.out = n.length))
@@ -17,68 +19,95 @@ mse.mat = matrix(0, ncol = trials, nrow = n.length)
 haus.mat = matrix(0, ncol = trials, nrow = n.length)
 jumps.mat = matrix(0, ncol = trials, nrow = n.length)
 
+simulation_suite <- function(trial){
+  set.seed(i*trial*10)
+    
+  y = generate.problem(n.vec[i], jump.mean.org, jump.location.org,  sigma)
+    
+  res = fusedlasso1d(y)
+  cv = cv.trendfilter(res, verbose = FALSE)
+    
+  fit = coef(res, lambda=cv$lambda.1se)$beta
+    
+  dist = compute.hausdorff(true.jumps, enumerate.jumps(fit), 
+                                          one.sided = TRUE)
+  haus = compute.hausdorff(true.jumps, enumerate.jumps(fit))
+  lambda = cv$lambda.1se
+  mse = compute.mse(fit, true.seq = truth)
+  jumps = count.jumps(fit)
+
+  #plot
+  png(paste0("~/DUMP/fused_lasso_n-", n.vec[i], "_trial-", trial, "_", DATE, ".png"), 
+   width = 8, height = 3, units = "in", res = 300)
+  plotfused(jump.mean.org, jump.location.org, y, fit, cv$lambda.1se, count.jumps(fit))
+  dev.off()
+ 
+  c(dist, haus, lambda, mse, jumps)
+}
+
 for(i in 1:n.length){
   truth = form.truth(jump.mean.org, jump.location.org, n.vec[i])
   true.jumps = enumerate.jumps(truth)
-  
-  for(trial in 1:trials){
-    set.seed(i*trial*10)
-    
-    y = generate.problem(n.vec[i], jump.mean.org, jump.location.org,  sigma)
-    
-    res = fusedlasso1d(y)
-    cv = cv.trendfilter(res, verbose = FALSE)
-    
-    fit = coef(res, lambda=cv$lambda.1se)$beta
-    
-    dist.mat[i,trial] = compute.hausdorff(true.jumps, enumerate.jumps(fit), 
-                                          one.sided = TRUE)
-    haus.mat[i,trial] = compute.hausdorff(true.jumps, enumerate.jumps(fit))
-    lambda.mat[i,trial] = cv$lambda.1se
-    mse.mat[i,trial] = compute.mse(fit, true.seq = truth)
-    jumps.mat[i,trial] = count.jumps(fit)
+ 
+  res.tmp = foreach(trial = 1:trials) %dopar% simulation_suite(trial)
+  res.tmp = do.call(cbind, res.tmp)  
 
-    res = list(dist.mat = dist.mat, lambda.mat = lambda.mat, mse.mat = mse.mat,
-     haus.mat = haus.mat, jumps.mat = jumps.mat, setup = setup)
-    save(res, file=paste0("~/DUMP/screening_experiment_", DATE, ".RData"))
-  }
-  
+  dist.mat[i,] = res.tmp[1,]
+  haus.mat[i,] = res.tmp[2,]
+  lambda.mat[i,] = res.tmp[3,]
+  mse.mat[i,] = res.tmp[4,]
+  jumps.mat[i,] = res.tmp[5,]
+
+  res = list(dist.mat = dist.mat, lambda.mat = lambda.mat, mse.mat = mse.mat,
+   haus.mat = haus.mat, jumps.mat = jumps.mat, setup = setup)
+  save(res, file=paste0("~/DUMP/screening_experiment_", DATE, ".RData"))
+ 
   cat('*')
 }
 
+
+png("~/DUMP/truth_2016-01-27.png", height = 5, width = 10, units = "in", res = 300)
+plot(y, col = rgb(.5,.5,.5), pch = 16, cex = 1.25)
+plot.helper(c(1,true.jumps,n.vec[i]), jump.mean.org, n.vec[i], col="red")
+dev.off()
 
 lambda.mat = res$lambda.mat
 n.vec = res$setup$n.vec
 logn.vec = log(n.vec, base = 10)
 
-#remove uncomputed rows
-idx = which(apply(lambda.mat, 1, function(x){all(x==0)}))
-if(length(idx)) {
-  lambda.mat = lambda.mat[-idx,]
-  n.vec = n.vec[-idx]
-}
-
 #take median
-lambda.vec = apply(lambda.mat, 1, function(x){median(x[x!=0])})
+lambda.vec = apply(lambda.mat, 1, mean)
+lambda.std = apply(lambda.mat, 1, sd)
 
-png("~/DUMP/lambda_2016-01-14.png", height = 5, width = 5, units = "in", res = 300)
+png("~/DUMP/screening_2016-01-14.png", height = 5, width = 10, units = "in", res = 300)
+
+par(mfrow = c(1,2))
+
 plot(x = n.vec, y = lambda.vec, pch = 16, cex = 2, xlab = "Length of vector (n)",
-     ylab = "Median Lambda value (1-se rule, 5-fold cv)")
+     ylab = "Median Lambda value (1-se rule, 5-fold cv)", ylim = c(0, max(lambda.vec+lambda.std)))
+for(i in 1:length(lambda.std)) {
+  lines(x = rep(n.vec[i], 2), y = c(lambda.vec[i] - lambda.std[i], lambda.vec[i] + lambda.std[i]),
+   cex = 2)
+}
 
 #form the best sqrt(n) fit
 x = seq(min(n.vec), max(n.vec), length.out = 1000)
 const = max(lambda.vec)/max(sqrt(x))
 y = const*sqrt(x)
 lines(x, y, col="red", lwd = 2)
-dev.off()
+#dev.off()
 
-mse.mat = res$mse.mat
-if(length(idx)) mse.mat = mse.mat[-idx,]
-mse.vec = apply(mse.mat, 1, function(x){median(x[x!=0])})
+mse.vec = apply(mse.mat, 1, mean)
+mse.std = apply(mse.mat, 1, sd)
 
-png("~/DUMP/mse_2016-01-14.png", height = 5, width = 5, units = "in", res = 300)
+#png("~/DUMP/mse_2016-01-14.png", height = 5, width = 5, units = "in", res = 300)
 plot(x = n.vec, y = mse.vec, pch = 16, cex = 2, xlab = "Length of vector (n)",
-     ylab = "Median MSE value (1-se rule, 5-fold cv)")
+     ylab = "Median MSE value (1-se rule, 5-fold cv)", ylim = c(0, max(mse.vec + mse.std)))
+for(i in 1:length(mse.std)) {
+  lines(x = rep(n.vec[i], 2), y = c(mse.vec[i] - mse.std[i], mse.vec[i] + mse.std[i]),
+   cex = 2)
+}
+
 const = min(mse.vec)/(min(log(x)*log(log(x))/x))
 y = const*log(x)*log(log(x))/x
 lines(x, y, col="red", lwd = 2)
--- source_header.R
+++ source_header.R
@@ -2,6 +2,8 @@ library(genlasso)
 library(assertthat)
 library(testthat)
 library(quadprog)
+library(doMC)
+library(foreach)
 
 source("source_main.R") #main fused lasso stuff
 source("source_plotter.R") #main plotting
